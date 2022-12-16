cd4c7435962b24522e658bebd85898091df3891f
Santiago Balmaceda <saantibalma@gmail.com>
2022-12-16 18:45:03 -0300

    Create goals model

--- app/src/main/java/com/santrucho/habilife/ui/navigation/Screen.kt
+++ app/src/main/java/com/santrucho/habilife/ui/navigation/Screen.kt
@@ -3,4 +3,5 @@ package com.santrucho.habilife.ui.navigation
 sealed class Screen(val route: String) {
     object LoginScreen : Screen("login_screen")
     object SignUpScreen : Screen("signup_screen")
+    object NewGoalScreen:Screen("new_goal_screen")
 }
