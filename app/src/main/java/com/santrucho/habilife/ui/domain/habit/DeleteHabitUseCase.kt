a961046775341ca0446e2114717efbc98f13143d
Santiago Balmaceda <saantibalma@gmail.com>
2023-04-17 18:44:48 -0300

    Added habit use cases test

--- app/src/main/java/com/santrucho/habilife/ui/domain/habit/DeleteHabitUseCase.kt
+++ app/src/main/java/com/santrucho/habilife/ui/domain/habit/DeleteHabitUseCase.kt
@@ -3,15 +3,17 @@ package com.santrucho.habilife.ui.domain.habit
 import android.util.Log
 import com.santrucho.habilife.ui.data.model.Habit
 import com.santrucho.habilife.ui.data.remote.habits.HabitsRepository
+import com.santrucho.habilife.ui.util.Resource
 import javax.inject.Inject
 
 class DeleteHabitUseCase @Inject constructor(private val repository:HabitsRepository) {
 
-    suspend operator fun invoke(habit: Habit){
-        try {
+    suspend operator fun invoke(habit: Habit) : Resource<Habit> {
+        return try {
             repository.deleteHabit(habit)
+            Resource.Success(habit)
         } catch (e:Exception){
-            Log.d("Error: ",e.message.toString())
+            Resource.Failure(e)
         }
     }
 }
